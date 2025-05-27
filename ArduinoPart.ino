96fa29f2c380f290aaf0c5aa9c455776ffe9a492
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-05-28 04:48:32 +0800

    Добавлен CO-датчик (аналоговый и цифровой выход)

--- ArduinoPart.ino
+++ ArduinoPart.ino
@@ -5,3 +5,8 @@ const int lightThreshold = 500;
 const int co2Pin = A3;
 int co2Value = 0;
 const int CO2_LIMIT = 600;  // допустимый уровень CO2
+
+#define ANALOG_PIN A4
+#define DIGITAL_PIN 8
+int coAnalogValue = 0;
+int coLimitStatus = 0;
