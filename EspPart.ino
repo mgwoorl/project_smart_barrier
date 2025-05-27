88f397c7f4d033a37b15060132268d1b0a381784
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-05-28 07:03:17 +0800

    Исправление ошибки

--- EspPart.ino
+++ EspPart.ino
@@ -3,7 +3,7 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 #include <Servo.h>
-
+ 
 const char* ssid = "Estriper";
 const char* password = "lalala3031";
 #define BOTtoken "8148150074:AAEtVWxB62mpDiIRsrUQJCHgci61_igmqRo"
