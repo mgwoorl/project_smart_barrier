e15578057cd9dff632cb627f112a2dd5d7282b2f
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-05-28 04:16:25 +0800

    Настройка Telegram-бота и инициализация клиентского подключения

--- EspPart.ino
+++ EspPart.ino
@@ -7,3 +7,6 @@
 const char* ssid = "Estriper";
 const char* password = "lalala3031";
 #define BOTtoken "8148150074:AAEtVWxB62mpDiIRsrUQJCHgci61_igmqRo"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
