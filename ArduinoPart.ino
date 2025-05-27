bafb61e98bf7cebb00745eb3aef5e8f9de43992a
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-05-28 04:47:29 +0800

    Добавлен аналоговый CO2-датчик с порогом безопасности

--- ArduinoPart.ino
+++ ArduinoPart.ino
@@ -1,3 +1,7 @@
 const int photoPin1 = A0;
 const int photoPin2 = A1;
 const int lightThreshold = 500;
+
+const int co2Pin = A3;
+int co2Value = 0;
+const int CO2_LIMIT = 600;  // допустимый уровень CO2
