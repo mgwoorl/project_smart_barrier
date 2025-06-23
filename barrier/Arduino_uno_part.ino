f30f96d98f2175275c9afafa28c8ed03ae29e33f
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-06-24 01:15:12 +0800

    добавлены loop и setup

--- barrier/Arduino_uno_part.ino
+++ barrier/Arduino_uno_part.ino
@@ -4,3 +4,22 @@ const int coPin = A4;
 
 const int lightThreshold = 500; 
 
+void setup() {
+  Serial.begin(9600);          
+}
+
+void loop() {
+  int light1 = analogRead(photoPin1);
+  int light2 = analogRead(photoPin2);
+  int coValue = analogRead(coPin);
+
+  String parking1 = (light1 < lightThreshold) ? "free" : "occupied";
+  String parking2 = (light2 < lightThreshold) ? "free" : "occupied";
+
+  String data = "CO:" + String(coValue);
+  data += ";P1:" + parking1;
+  data += ";P2:" + parking2;
+
+  Serial.println(data);
+  delay(1000);
+}
