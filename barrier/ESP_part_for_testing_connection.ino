9d8e261717ca349139dee2ff14de2c8aad9332ce
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-06-24 11:32:36 +0800

    объявление переменных для esp

--- barrier/ESP_part_for_testing_connection.ino
+++ barrier/ESP_part_for_testing_connection.ino
@@ -1,3 +1,29 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Servo.h>
+
+const char* ssid = "Estriper";
+const char* password = "lalala3031";
+
+const char* baseUrl = "http://192.168.109.122:5050";
+const char* dataEndpoint = "/sensors/data";
+const char* resetEndpoint = "/sensors/reset-gate-status";
+const int espId = 1;
+
+const int servoPin = D4;
+
+Servo barrierServo;
+WiFiClient client;
+
+bool isGateOpened = false;
+unsigned long gateOpenedAt = 0;
+unsigned long lastStatusCheck = 0;
+unsigned long lastDataSend = 0;
+
+int freePlaces = -1;
+int co2Value = -1;
+int distanceEntrance = 25;
+int distanceExit = 25;
+
+enum GateAction { NONE, ENTRANCE, EXIT };
+GateAction lastGateAction = NONE;
