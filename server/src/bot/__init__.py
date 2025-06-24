895b71b43f165f4ecb38304c587186ad40e171e2
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-06-24 09:54:37 +0800

    Добавление новых функций в __init__.py

--- server/src/bot/__init__.py
+++ server/src/bot/__init__.py
@@ -1,1 +1,5 @@
 from .logic import _add_new_user
+from .logic import _change_user_status
+from .logic import _remove_admin_rights
+from .logic import _set_wanna_entrance_open
+from .logic import _set_wanna_exit_open
