c99b04fcdcce7d7d73e92cb628185804b756edc0
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-06-24 11:23:54 +0800

    Реализована логика добавления или обновления записи датчика по ID. Если запись существует то обновляются значения, иначе создаётся новая.

--- server/src/sensors/logic.py
+++ server/src/sensors/logic.py
@@ -1,1 +1,29 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.sensors.models import Sensor, System
+from src.sensors.schemes import ESPDataModel, ResetGateStatusModel
+from src.sensors.exceptions import SensorException
 
+
+async def upsert_sensor_data(data: ESPDataModel, db: AsyncSession):
+    result = await db.execute(select(Sensor).filter(Sensor.id == data.id))
+    sensor = result.scalar_one_or_none()
+
+    if sensor:
+        sensor.co2 = data.co2
+        sensor.free_places = data.free_places
+        sensor.distance_entrance = data.distance_entrance
+        sensor.distance_exit = data.distance_exit
+    else:
+        sensor = Sensor(
+            id=data.id,
+            co2=data.co2,
+            free_places=data.free_places,
+            distance_entrance=data.distance_entrance,
+            distance_exit=data.distance_exit
+        )
+        db.add(sensor)
+
+    await db.commit()
+    await db.refresh(sensor)
+    return sensor
