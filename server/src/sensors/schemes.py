033a7b3278b1a6a19ec9601f8d3a4eb00caba2ee
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-06-25 03:48:57 +0800

    Добавлены модели данных для взаимодействия с ESP валидации через Pydantic

--- server/src/sensors/schemes.py
+++ server/src/sensors/schemes.py
@@ -1,1 +1,18 @@
+from pydantic import BaseModel
 
+class ESPDataModel(BaseModel):
+    id: int  # ESP ID — будет primary key для обновления
+    distance_exit: int | None = None
+    distance_entrance: int | None = None
+    free_places: int
+    co2: int
+
+class SimpleResponseModel(BaseModel):
+    detail: str
+
+from pydantic import BaseModel
+from typing import Optional
+
+class ResetGateStatusModel(BaseModel):
+    reset_entrance: Optional[bool] = False
+    reset_exit: Optional[bool] = False
