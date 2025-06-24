0fcf8ead1e922c2ffe81f894ce0aae6e9321bfe6
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-06-24 11:08:35 +0800

    Добавлен класс User

--- server/src/sensors/models.py
+++ server/src/sensors/models.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from src.database import BaseDBModel
 
+class User(BaseDBModel):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chat_id = Column(String(255), nullable=False)
+    isAdmin = Column(Boolean, nullable=False)
+    deleted_at = Column(DateTime, nullable=True)
+
