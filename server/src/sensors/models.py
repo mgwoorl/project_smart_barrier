895836f62b0e87797582385fbd3a6202949df40e
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-06-24 11:08:14 +0800

    Добавлены импорты в models.py

--- server/src/sensors/models.py
+++ server/src/sensors/models.py
@@ -1,1 +1,5 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from src.database import BaseDBModel
 
