21608d9d31effd4c0d313e8dee86deab7d8f0697
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-06-25 10:06:39 +0800

    Добавлены импорты в logic.py

--- server/src/sensors/models.py
+++ server/src/sensors/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from src.database import BaseDBModel
+from sqlalchemy import Date, Time
 
 class User(BaseDBModel):
     __tablename__ = "users"
